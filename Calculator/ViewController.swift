1d78eb3db299a773e3a0059eacedc3ae85102463
Ramesh <rameshjakhar199929@gmail.com>
2023-08-03 01:14:19 +0530

    calculatorUIReady

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var calculatorResult: UILabel!
+    @IBOutlet weak var calculatorWorking: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func equalTap(_ sender: Any) {
+    }
+    @IBAction func allClearTap(_ sender: Any) {
+    }
+    
+    @IBAction func backTap(_ sender: Any) {
+    }
+    @IBAction func percentTap(_ sender: Any) {
+    }
+    
+    
+    @IBAction func divideTap(_ sender: Any) {
+    }
+    
+    @IBAction func multiplyTap(_ sender: Any) {
+    }
+    @IBAction func subtractTap(_ sender: Any) {
+    }
+    
+    @IBAction func addTap(_ sender: Any) {
+    }
+    
+    
+    @IBAction func nineTap(_ sender: Any) {
+    }
+    
+    @IBAction func eightTap(_ sender: Any) {
+    }
+    
+    @IBAction func sevenTap(_ sender: Any) {
+    }
+    
+    
+    @IBAction func sixTap(_ sender: Any) {
+    }
+    
+    @IBAction func fiveTap(_ sender: Any) {
+    }
+    @IBAction func fourTap(_ sender: Any) {
+    }
+    @IBAction func threeTap(_ sender: Any) {
+    }
+    @IBAction func twoTap(_ sender: Any) {
+    }
+    @IBAction func oneTap(_ sender: Any) {
+    }
+    
+    @IBAction func zeroTap(_ sender: Any) {
+    }
+    
+    @IBAction func decimalTap(_ sender: Any) {
+    }
 }
 
